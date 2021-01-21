2d30403e932f466de3559a3f8d7447d1977cc54a
Android-Penguin <mail@shauncb.xyz>
2021-01-21 14:48:50 +1300

    Top and Bottom Gradients
    - Added header and footer gradients.
    
    - Added base nav bar (no links).

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import titleVector from './images/titleGradient.svg';
+import footerVector from './images/footerGradient.svg';
 import './App.css';
+
+function TitleBox(props) {
+  return (
+    <div id={props.divID} className="titleDiv">
+      <div className="navBarDiv">
+        <ul className="navBar">
+          <li><a>Home</a></li>
+          <li>|</li>
+          <li><a>About</a></li>
+          <li>|</li>
+          <li><a>Products</a></li>
+          <li>|</li>
+          <li><a>Contact Us</a></li>
+        </ul>
+      </div>
+      <div className="titleGraphic"></div>
+    </div>
+  )
+}
+
+function ContentBox(props) {
+  return (
+    <div className="contentParentDiv">
+      
+    </div>
+  )
+}
+
+function FooterBox(props) {
+  return (
+    <div className="footerDiv">
+      <div className="footerGraphic"></div>
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="parentDiv">
+      <TitleBox divID="homeTitleDiv"/>
+      <ContentBox />
+      <FooterBox />
     </div>
   );
 }
